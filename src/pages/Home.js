26332d5306b35cff7521ac7379893ba79ecd367c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-06-18 22:36:16 -0400

    shared layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 const Home = () => {
   return (
-    <section className="section">
-      <h2>Home Page</h2>
-      <Link to="/about" className="btn">
+    <>
+      <Navbar />
+      <section className="section">
+        {/* <Link to="/about" className="btn">
         About
-      </Link>
-    </section>
+      </Link> */}
+        <Outlet />
+      </section>
+    </>
   );
 };
 export default Home;
