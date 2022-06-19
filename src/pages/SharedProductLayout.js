64bd614d0d315ba283ef742cf706556eca9ee55f
Fc0S4b <fcoj.sabugo@gmail.com>
2022-06-19 02:55:54 -0400

    product page refactor

---
+++ src/pages/SharedProductLayout.js
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+const SharedLayout = () => {
+  return (
+    <>
+      <h2>products</h2>
+      <Outlet />
+    </>
+  );
+};
+export default SharedLayout;
